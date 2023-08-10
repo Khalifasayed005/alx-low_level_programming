4038490280802fbb6ef357e7d7bb46f9b2891f66
Khalifasayed005 <khalifa.sayed.005@gmail.com>
2023-08-10 13:58:21 -0700

    j

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
+
 /**
-*main - entery poit
-*return : alwaays 0 (success)
-*/
+ * main - Entery point
+ *
+ * Description: A C program that print with put function
+ *
+ * Return : always 0 (success)
+ */
+
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
