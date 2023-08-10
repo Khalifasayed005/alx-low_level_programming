fe8c68c1ad2cbb88b3761ca35ef87d3d5b702a87
Khalifasayed005 <khalifa.sayed.005@gmail.com>
2023-08-10 15:18:51 -0700

    j

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
 #include <unistd.h>
 /**
  * main - Entery point
- * Description: A C program that print with put function
- * Return: 1 (error)
+ * Description: print a qoute using write function
+ * ssize_t write(int f, const void *buf, size_t count);
+ * Return: 1 (error).
  */
 int main(void)
 {
 char qou[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(1. qou. 59);
+	write(1, qou, 59);
 return (1);
 }
