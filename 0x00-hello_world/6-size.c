72fde3b4ced722bcf9bd6337dc267c8261eecd00
Khalifasayed005 <khalifa.sayed.005@gmail.com>
2023-08-10 14:34:06 -0700

    j

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,6 +10,6 @@ printf("Size of a char: %u byte(s)\n", sizeof(char));
 printf("Size of a int: %u byte(s)\n", sizeof(int));
 printf("Size of a long int: %u byte(s)\n", sizeof(long int));
 printf("Size of a long long in: %u byte(s)\n", sizeof(long long int));
-printf("Size of afloat: %u byte(s)\n", sizeof(float));
+printf("Size of a float: %u byte(s)\n", sizeof(float));
 return (0);
 }
