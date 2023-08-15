56ce4185a85f7a7e541bf22978053b9cad12da26
Khalifasayed005 <khalifa.sayed.005@gmail.com>
2023-08-15 16:53:43 -0700

    j

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - Adds two integers
+ * @a: The first number to be computed
+ * @b: The second number to be computed
+ * Return: sum of a & b
+*/
+
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}
