8565c98a54863a36d939e4dcee750f26d6453dba
Khalifasayed005 <khalifa.sayed.005@gmail.com>
2023-08-15 19:35:42 -0700

    j

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - print first 50 Fibonacci numbers, starting with  and
+ * Return: Always 0 (success)
+ * separated by comma followed by a space
+*/
+
+int main(void)
+{
+	int count;
+	unsigned long fib1 = 0, fib2 = 1, sum;
+
+	for (count = 0; count < 50; count++)
+	{
+		sum = fib1 + fib2;
+		printf("%lu", sum);
+
+		fib1 = fib2;
+		fib2 = sum;
+
+		if (count == 49)
+			printf("\n");
+		else
+			printf(", ");
+	}
+	return (0);
+}
