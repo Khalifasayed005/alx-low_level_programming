dd47668f81a80e0c41ed62805753750c35596be2
Khalifasayed005 <khalifa.sayed.005@gmail.com>
2023-08-21 12:24:05 -0700

    j

--- 0x05-pointers_arrays_strings/_putchar.c
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -1,12 +1,13 @@
 #include <unistd.h>
 
-/**
- * _putchar - writes the character c to print
- * @c: the character to print 
- * Return: on success 1.
- * on error, -1 is rturned, and errno is set appropristely
-*/
 
+/**
+ * _putchar - Writes the chracter c to stdout
+ * @c: The charcter to print
+ *
+ * Return: On success 1.
+ * On error, -1 is return, and errno is set appropriately
+*/
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
