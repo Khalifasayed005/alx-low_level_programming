4b9838eda9fcf6be9d474279783f86f058e50fcf
Khalifasayed005 <khalifa.sayed.005@gmail.com>
2023-08-24 18:51:21 -0700

    j

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -13,13 +13,13 @@ void print_number(int n)
 	a = n;
 	if (n < 0)
 	{
-		_putchar('-');
+		putchar('-');
 		a = -n;
 	}
 	if (a / 10 != 0)
 	{
 		print_number(a / 10);
 	}
-	_putchar((a % 10) + '0')
+	putchar((a % 10) + '0')
 }
 
