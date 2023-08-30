d92bc56dbe14a05a8a54378799d9f65d85fa0278
Khalifasayed005 <khalifa.sayed.005@gmail.com>
2023-08-30 13:42:39 -0700

    j

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -3,15 +3,15 @@
 
 #include <stdio.h>
 
-int putchar(char c);
-void puts_recursion(char *s);
-void print rev recursion(char *s);
-int strlen recursion(char *s);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
 int factorial(int n);
-int pow recursion(int x, int y);
-int _sqrt recursion(int n);
-int is prime number(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
+
 
 #endif /* MAIN_H */
