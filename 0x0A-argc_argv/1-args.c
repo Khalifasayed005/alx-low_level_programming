0f24cb65d14a5aba708a1adbec8591bb9797a982
Khalifasayed005 <khalifa.sayed.005@gmail.com>
2023-09-01 10:17:30 -0700

    j

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints the argument
+ * @argc: int
+ * @argv: list
+ * Return: 0-success, non-zero-fail
+*/
+
+int main(int argc, char const *argv[])
+{
+(void)argv;
+printf("%d\n", argc - 1]);
+return (0);
+}
---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - rints its name, followed by a new line
+ * @argc: int
+ * @argv: list
+ * Return: 0
+*/
+
+int main(int argc, char const *argv[])
+{
+int sum;
+
+if (argc == 3)
+{
+sum = atoi(argv[1]) * atoi(argv[2]);
+printf("%d\n", sum);
+}
+else
+{
+printf("Error\n");
+
+return (0);
+}
