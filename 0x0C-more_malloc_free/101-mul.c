32c595b3eceecae7e838b19eef5b6bf11272ef46
Khalifasayed005 <khalifa.sayed.005@gmail.com>
2023-09-07 13:45:55 -0700

    j

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -26,7 +26,7 @@ int _atoi(const char *s)
 	int sign = 1;
 	unsigned long int resp = 0, firstNum, i;
 
-	for (firstNum = 0; (s[firstNum] >= 48 && s[firstNum] <= 57); firstNum++)
+	for (firstNum = 0; !(s[firstNum] >= 48 && s[firstNum] <= 57); firstNum++)
 	{
 		if (s[firstNum] == '-')
 		{
