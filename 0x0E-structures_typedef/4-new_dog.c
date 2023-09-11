32eca30f8537b87ac1f9a927bf65ab921d7d2c3b
Khalifasayed005 <khalifa.sayed.005@gmail.com>
2023-09-11 14:36:11 -0700

    j

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,21 @@
 #include <stdlib.h>
 #include "dog.h"
+/**
+ * _strlen - a function that gets a length of string
+ *
+ * @str: the string to get the length
+ *
+ * Return: length of @str
+*/
+
+int _strlen(const char *str)
+{
+	int length = 0;
+
+	while (*str++)
+		length++;
+	return (length);
+}
 
 /**
  * _strcopy a function that returns @dest with a copy of a string from @src
