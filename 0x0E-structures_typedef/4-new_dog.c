5fba75f30e85281a7c0a59c651c382429fc74eb8
Khalifasayed005 <khalifa.sayed.005@gmail.com>
2023-09-11 15:05:01 -0700

    j

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -43,9 +43,10 @@ return (dest);
  * *new_dog - creates a new dog.
  * @name: dog name
  * @age: dog age
- * @owner: owner na,e
+ * @owner: dog owner
  *
- * Return: void
+ * Return: struct pointer dog
+ * NULL if function fails
 */
 
 dog_t *new_dog(char *name, float age, char *owner)
