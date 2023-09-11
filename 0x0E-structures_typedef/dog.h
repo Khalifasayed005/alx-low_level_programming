758ecc38656f5ad358408cf94575e1cdae950742
Khalifasayed005 <khalifa.sayed.005@gmail.com>
2023-09-11 14:22:51 -0700

    j

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef dog_H
-#define dog_H
+#ifndef DOG_H
+#define DOG_H
 
 /**
  * struct dog - a dog struct
@@ -16,7 +16,7 @@ struct dog
 	float age;
 	char *owner;
 };
-typedef struct dog dog_t
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
