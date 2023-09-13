533f2cb8bb953ddb01906223be9174d764156cb5
Khalifasayed005 <khalifa.sayed.005@gmail.com>
2023-09-13 12:53:06 -0700

    j

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,65 @@
+#include "function_pointers.h"
+
+}
+/**
+ * op_add - adds two integers
+ * @a: the first int
+ * @b: the second int
+ *
+ *
+ * Return: the sum
+*/
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+}
+/**
+ * op_sub - subtract two integers
+ * @a: the first int
+ * @b: the second int
+ *
+ *
+ * Return: the difference
+*/
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multilies two integers
+ * @a: the first int
+ * @b: the second int
+ *
+ *
+ * Return: the product
+*/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - devides two integers
+ * @a: the first int
+ * @b: the second int
+ * 
+ *
+ * Return: the quotient
+*/
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - mudulo two integers
+ * @a: the first int
+ * @b: the second int
+ * 
+ *
+ * Return: the remainder
+*/
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
