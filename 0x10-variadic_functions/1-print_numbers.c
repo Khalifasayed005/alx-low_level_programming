015ca3d955a3332eeec86f8acc998c39c9a56e2b
Khalifasayed005 <khalifa.sayed.005@gmail.com>
2023-09-14 10:42:34 -0700

    j

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+
+/**
+* print_numbers -  prints numbers, followed by a new line.
+* @separator: the string separator
+* @n: the number of argument
+* @...: the integers to sum
+* Return: the integer sum
+*/
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	int i = n;
+	va_list ap;
+
+	if (!n)
+	{
+		printf("\n");
+		return;
+	}
+	va_start(ap, n);
+	while (i--)
+		printf("%d%s", va_arg(ap, int),
+				! ? (separator ? separator : "") : "\n");
+	va_end(ap);
+}
