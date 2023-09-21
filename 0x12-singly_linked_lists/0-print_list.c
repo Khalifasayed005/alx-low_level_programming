fbe74027cb68d5b06fad4e9ac491dabc0aad54e0
Khalifasayed005 <khalifa.sayed.005@gmail.com>
2023-09-21 16:36:59 -0700

    j

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -30,7 +30,7 @@ size_t i = 0;
 while (h)
 {
 
-printf("[%d] %s\n", _strlen(h->str), h->str ? h->str : "(nil)");
+printf("[%u] %s\n", _strlen(h->str), h->str ? h->str : "(nil)");
 h = h->next;
 i++;
 }
