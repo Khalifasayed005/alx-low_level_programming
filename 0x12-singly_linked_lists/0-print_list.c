3ecded6dfa736a0c694f22172343a7a21072c08a
Khalifasayed005 <khalifa.sayed.005@gmail.com>
2023-09-21 17:23:09 -0700

    j

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,7 +2,7 @@
 #include "lists.h"
 
 /**
-* strlen - returns the length of a string
+* _strlen - returns the length of a string
 * @s: the string whose length to check
 * Return: integer length of string
 */
