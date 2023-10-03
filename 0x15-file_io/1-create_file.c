bf7de178f4d0c288b88b11495adabf489c2ed272
Khalifasayed005 <khalifa.sayed.005@gmail.com>
2023-10-02 17:57:22 -0700

    j

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -28,14 +28,13 @@ return (i);
 int create_file(const char *filename, char *text_content)
 {
 int fd;
-ssize_t bytes = 0, len = strlen(text_content);
+ssize_t bytes = 0, len = _strlen(text_content);
 
 if (!filename)
 return (-1);
 fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 if (fd == -1)
 return (-1);
-
 if (len)
 bytes = write(fd, text_content, len);
 close(fd);
