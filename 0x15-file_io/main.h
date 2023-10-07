35da65d514cd4bd3c9aceaa3ba660cbaa77e613c
Khalifasayed005 <khalifa.sayed.005@gmail.com>
2023-10-06 17:17:53 -0700

    j

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -13,12 +13,5 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 void print_type(Elf64_Ehdr h);
-void print_abiversion(Elf64_Ehdr h);
-void print_osabi_more(Elf64_Ehdr h);
-void print_osabi(Elf64_Ehdr h);
-void print_version(Elf64_Ehdr h);
-void print_data(Elf64_Ehdr h);
-void print_class(Elf64_Ehdr h);
-void print_magic(Elf64_Ehdr h);
 
 #endif
